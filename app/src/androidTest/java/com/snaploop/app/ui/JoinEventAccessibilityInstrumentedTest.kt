c71b64e10ef51cddc306563330ad645cfcf8fc20
gurmeetgold <50425243+gurmeetgold@users.noreply.github.com>
2026-09-09 17:59:32 -0400

    Fix Join accessibility instrumentation import

--- app/src/androidTest/java/com/snaploop/app/ui/JoinEventAccessibilityInstrumentedTest.kt
+++ app/src/androidTest/java/com/snaploop/app/ui/JoinEventAccessibilityInstrumentedTest.kt
@@ -1,7 +1,6 @@
 package com.snaploop.app.ui
 
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.test.assertExists
 import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
