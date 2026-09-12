c495f4c24d2c0ca8415b0ae4aa8325d7c9eaf7eb
gurmeetgold <50425243+gurmeetgold@users.noreply.github.com>
2026-09-12 12:25:53 -0400

    Bump Android internal release after ONNX crash fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = snapLoopApplicationId
         minSdk = 26
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
         buildConfigField("boolean", "FIREBASE_CONFIG_PRESENT", hasFirebaseConfig.toString())
