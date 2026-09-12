3b0107993dedce4e55116cfb7e2803681830ce06
gurmeetgold <50425243+gurmeetgold@users.noreply.github.com>
2026-09-12 11:45:48 -0400

    Bump Android internal release to 1.0.1 (2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = snapLoopApplicationId
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
         buildConfigField("boolean", "FIREBASE_CONFIG_PRESENT", hasFirebaseConfig.toString())
